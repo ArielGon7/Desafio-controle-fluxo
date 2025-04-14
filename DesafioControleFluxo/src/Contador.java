2c7e33846deda2eec44711d9d4a991f20618eeb2
ArielGon7 <arielgomessantos7@gmail.com>
2025-04-14 14:45:36 -0300

    Update Contador.java
    Segundo commit remote

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -24,7 +24,7 @@ public class Contador {
 		}
 		int contagem = parametroDois - parametroUm;
 		for(int i=1;i<=contagem;i++) {
-			System.out.println("Ocorrências: " + i);
+			System.out.println("Imprimindo numero: " + i);
 		}
 		
 		
